f494031cee7233893375f9e05d2127c4d00aa672
Vincent Cramer <cramervincent@gmail.com>
2023-02-05 17:11:42 +0100

    base with sass

--- src/views/HomeLayout.vue
+++ src/views/HomeLayout.vue
@@ -1,32 +1,44 @@
 <template>
-     <div class="info-bar">
-        <div class="container">
-            <ul class="usp">
-                <li>Duitse prijzen</li>
-                <li>A kwaliteit</li>
-                <li>Snelle levering</li>
-            </ul>
-        </div>
+  <div class="info-bar">
+    <div class="container">
+      <ul class="usp">
+        <li>Duitse prijzen</li>
+        <li>A kwaliteit</li>
+        <li>Snelle levering</li>
+      </ul>
     </div>
-   <header>
-    <div class="menu">
-        <div class="container">
-            <ul>
-                <li>menu item 1</li>
-                <li>menu item 2</li>
-                <li>menu item 3</li>
-                <li>menu item 4</li>
-                <li>menu item 5</li>
-            </ul>
-        </div>
+  </div>
+  <header>
+    <div class="menu" @click="toggleMenu">
+      <div class="container">
+        <ul>
+          <li>menu item 1</li>
+          <li>menu item 2</li>
+          <li>menu item 3</li>
+          <li>menu item 4</li>
+          <li>menu item 5</li>
+        </ul>
+      </div>
     </div>
     <div class="navbar">
-        <div class="menu-toggle">|||</div>
-        <div class="logo">LOGO</div>
-        <div class="shopping_basket">B</div>
+      <div class="menu-toggle" @click="toggleMenu">|||</div>
+      <div class="logo">LOGO</div>
+      <div class="shopping_basket">B</div>
     </div>
-   </header>
-<main><router-view/></main>
-<footer>footer</footer>
+  </header>
+  <main><router-view /></main>
+  <footer>footer</footer>
 </template>
 
+<script>
+export default {
+    methods:{
+        toggleMenu(){
+            let menu = document.querySelector('.menu')
+            menu.classList.toggle('show');
+            }
+
+        }
+    }
+
+</script>
